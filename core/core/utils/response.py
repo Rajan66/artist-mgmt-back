ab226d35134a4cbc2963f40a69c54075f37f0cd7
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-14 10:09:17 +0545

    feat(base): add choices

--- core/core/utils/response.py
+++ core/core/utils/response.py
@@ -2,12 +2,29 @@ from rest_framework import status
 from rest_framework.response import Response
 
 
-def success_response(data=None, message="Success", status=status.HTTP_200_OK):
-    return Response({"data": data, "message": message}, status=status)
+def success_response(
+    data=None,
+    message="Success",
+    status=status.HTTP_200_OK,
+):
+    return Response(
+        {
+            "data": data,
+            "message": message,
+        },
+        status=status,
+    )
 
 
 def error_response(
-    error=None, message="Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR
+    error=None,
+    message="Error",
+    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
 ):
-    print(error, message, status)
-    return Response({"error": error, "message": message}, status=status)
+    return Response(
+        {
+            "error": error,
+            "message": message,
+        },
+        status=status,
+    )
