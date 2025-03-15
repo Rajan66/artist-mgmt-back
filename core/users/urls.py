604e172ec4c9be195473ef653c210264050d0bce
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-15 22:44:34 +0545

    feat(profile): implement views, urls and serializers for user profile

--- core/users/urls.py
+++ core/users/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from users.views.user import UserDetailView, UserListView
+from users.views.user_profile import ProfileDetailView, ProfileListView
 
 urlpatterns = [
+    path(
+        "<str:user_id>/profile/",
+        ProfileDetailView.as_view(),
+        name="user-profile-detail",
+    ),
+    path("profile/", ProfileListView.as_view(), name="user-profile-list"),
     path("<str:pk>/", UserDetailView.as_view(), name="user-detail"),
     path("", UserListView.as_view(), name="user-list"),
 ]
