0c71b9dfa52ee363d4938bdb17fb32336261b785
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-15 22:44:11 +0545

    feat(profile): implement CRUD for user profile

--- core/users/services/user_profile.py
+++ core/users/services/user_profile.py
@@ -1,44 +1,118 @@
 import uuid
 
-from django.db import connection
+from django.db import DatabaseError, connection
 from django.utils import timezone
 from rest_framework import status
+from users.serializers import UserOutputSerializer, UserProfileSerializer
 
-from core.base.choices import GenderChoices
-from core.utils.response import success_response
+from core.utils.response import error_response, success_response
 
 
 class UserProfileService:
     def get_profiles(self):
-        pass
+        try:
+            with connection.cursor() as c:
+                c.execute("SELECT * FROM users_user_profile")
+                results = c.fetchall()
 
-    def get_profile(self):
-        pass
+                columns = [col[0] for col in c.description]
+
+                profile_dicts = [dict(zip(columns, row)) for row in results]
+
+                for profile in profile_dicts:
+                    user_id = profile.get("user_id")
+                    # TODO move raw query code to selectors
+                    c.execute("SELECT * FROM users_customuser WHERE id=%s", [user_id])
+                    result = c.fetchone()
+
+                    if not result:
+                        return error_response(
+                            error="Invalid user ID",
+                            message="User not found",
+                            status=status.HTTP_404_NOT_FOUND,
+                        )
+
+                    columns = [col[0] for col in c.description]
+                    user_dicts = dict(zip(columns, result))
+                    serializer = UserOutputSerializer(user_dicts)
+                    profile["user"] = serializer.data
+
+            serializer = UserProfileSerializer(profile_dicts, many=True)
+            profiles = serializer.data
+
+            return success_response(
+                profiles,
+                message="User profiles found successfully",
+                status=status.HTTP_200_OK,
+            )
+
+        except Exception as e:
+            return error_response(
+                error=str(e),
+                message="User profile list retrieval failed",
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+    def get_profile(self, user_id):
+        try:
+            with connection.cursor() as c:
+                c.execute(
+                    "SELECT * FROM users_user_profile WHERE user_id=%s", [user_id]
+                )
+                result = c.fetchone()
+
+                columns = [col[0] for col in c.description]
+                profile_dicts = dict(zip(columns, result))
+
+                c.execute("SELECT * FROM users_customuser WHERE id=%s", [user_id])
+                result = c.fetchone()
+
+                if not result:
+                    return error_response(
+                        error="Invalid user ID",
+                        message="User not found",
+                        status=status.HTTP_404_NOT_FOUND,
+                    )
+
+                columns = [col[0] for col in c.description]
+                user_dicts = dict(zip(columns, result))
+                serializer = UserOutputSerializer(user_dicts)
+                profile_dicts["user"] = serializer.data
+
+            serializer = UserProfileSerializer(profile_dicts)
+            profile = serializer.data
+
+            return success_response(
+                profile,
+                message="User profiles found successfully",
+                status=status.HTTP_200_OK,
+            )
+
+        except Exception as e:
+            return error_response(
+                error=str(e),
+                message="User profile list retrieval failed",
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
     def create(self, payload, user_id):
+        # TODO add transcation and kill user creation if profile fails
         try:
             payload = payload or {}
-            id = payload.get("id") if payload.get("id") else uuid.uuid4()
-            first_name = payload.get("first_name") if payload.get("first_name") else ""
-            last_name = payload.get("last_name") if payload.get("last_name") else ""
-            dob = payload.get("dob") if payload.get("dob") else None
-            gender = (
-                payload.get("gender") if payload.get("gender") else GenderChoices.MALE
-            )
-            address = payload.get("address") if payload.get("address") else ""
-            phone = payload.get("phone") if payload.get("phone") else ""
-            created_at = (
-                payload.get("created_at")
-                if payload.get("created_at")
-                else timezone.now()
-            )
+            id = payload.get("id", uuid.uuid4())
+            first_name = payload.get("first_name", "")
+            last_name = payload.get("last_name", "")
+            dob = payload.get("dob", None)
+            gender = payload.get("gender", None)
+            address = payload.get("address", "")
+            phone = payload.get("phone", "")
+            created_at = payload.get("created_at", timezone.now())
             updated_at = timezone.now()
-            print("User profile creation is running.........")
 
             with connection.cursor() as c:
                 c.execute(
                     """INSERT INTO users_user_profile (id, first_name, last_name, dob, gender, address, phone, created_at, updated_at, user_id)
-                    values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING *;
                     """,
                     [
                         id,
@@ -57,25 +131,150 @@ class UserProfileService:
                 if not result:
                     raise Exception("User profile creation failed")
 
-                print("Profile rows==========>", result)
                 columns = []
                 for col in c.description:
                     columns.append(col[0])
 
             profile_dicts = dict(zip(columns, result))
-            print("Profile dict ==============>", profile_dicts)
+            user_id = profile_dicts.get("user_id")
+
+            with connection.cursor() as c:
+                c.execute("SELECT * FROM users_customuser WHERE id=%s", [user_id])
+                result = c.fetchone()
+
+                if not result:
+                    return error_response(
+                        error="Invalid user ID",
+                        message="User not found",
+                        status=status.HTTP_404_NOT_FOUND,
+                    )
+
+                columns = [col[0] for col in c.description]
+                user_dicts = dict(zip(columns, result))
+                serializer = UserOutputSerializer(user_dicts)
+                profile_dicts["user"] = serializer.data
+
+            serializer = UserProfileSerializer(profile_dicts)
+            profile = serializer.data
 
             return success_response(
-                "something is getting sent..",
+                profile,
                 message="User profile created succesfully",
                 status=status.HTTP_201_CREATED,
             )
 
         except Exception as e:
-            print(e)
+            return error_response(
+                error=str(e),
+                message="User profile creation failed",
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
-    def update(self):
-        pass
+    def update(self, payload, user_id):
+        try:
+            with connection.cursor() as c:
+                c.execute(
+                    "SELECT * FROM users_user_profile WHERE user_id=%s",
+                    [user_id],
+                )
+                result = c.fetchone()
+                if not result:
+                    raise Exception("Profile not found for this user")
 
-    def delete(self):
-        pass
+                columns = []
+                for col in c.description:
+                    columns.append(col[0])
+
+                old_profile = dict(zip(columns, result))
+                first_name = payload.get("first_name", old_profile.get("first_name"))
+                last_name = payload.get("last_name", old_profile.get("last_name"))
+                dob = payload.get("dob", old_profile.get("dob"))
+                gender = payload.get("gender", old_profile.get("gender"))
+                address = payload.get("address", old_profile.get("address"))
+                phone = payload.get("phone", old_profile.get("phone"))
+                updated_at = timezone.now()
+
+                c.execute(
+                    """UPDATE users_user_profile SET
+                    first_name=%s, last_name=%s, dob=%s, gender=%s, address=%s, phone=%s, updated_at=%s 
+                    WHERE user_id=%s RETURNING *;
+                    """,
+                    [
+                        first_name,
+                        last_name,
+                        dob,
+                        gender,
+                        address,
+                        phone,
+                        updated_at,
+                        user_id,
+                    ],
+                )
+                result = c.fetchone()
+                if not result:
+                    raise Exception("User profile updation failed")
+
+                columns = []
+                for col in c.description:
+                    columns.append(col[0])
+
+            profile_dicts = dict(zip(columns, result))
+            with connection.cursor() as c:
+                c.execute("SELECT * FROM users_customuser WHERE id=%s", [user_id])
+                result = c.fetchone()
+
+                if not result:
+                    return error_response(
+                        error="Invalid user ID",
+                        message="User not found",
+                        status=status.HTTP_404_NOT_FOUND,
+                    )
+
+                columns = [col[0] for col in c.description]
+                user_dicts = dict(zip(columns, result))
+                serializer = UserOutputSerializer(user_dicts)
+                profile_dicts["user"] = serializer.data
+
+            serializer = UserProfileSerializer(profile_dicts)
+            profile = serializer.data
+
+            return success_response(
+                profile,
+                message="User profile updated succesfully",
+                status=status.HTTP_200_OK,
+            )
+
+        except Exception as e:
+            return error_response(
+                error=str(e),
+                message="User profile updation failed",
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+    def delete(self, id):
+        try:
+            with connection.cursor() as c:
+                c.execute(
+                    "DELETE FROM users_user_profile WHERE user_id=%s RETURNING TRUE;",
+                    [id],
+                )
+                result = c.fetchone()
+
+                if not result:
+                    return error_response(
+                        error="Invalid user ID",
+                        message="Profile does not exist",
+                        status=status.HTTP_404_NOT_FOUND,
+                    )
+
+            return success_response(
+                message="User profile deleted successfully",
+                status=status.HTTP_204_NO_CONTENT,
+            )
+
+        except DatabaseError as e:
+            return error_response(
+                error=str(e),
+                message="Database error",
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
