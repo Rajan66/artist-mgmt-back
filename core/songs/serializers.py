9520f1ca5e6b2bdf02fe2cd8b019154979b177c3
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-17 22:40:21 +0545

    fix(songs): remove songs app

--- core/songs/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-
-class SongSerializer(serializers.Serializer):
-    id = serializers.UUIDField(read_only=True)
-    title = serializers.CharField()
-    album_name = serializers.CharField()
-    genre = serializers.CharField()
