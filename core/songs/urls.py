f823398daadae57088e53ceee107b87252e97462
Rajan66 <rajanmaharjan042@gmail.com>
2025-03-31 17:33:04 +0545

    feat(songs): implement managers api for songs

--- core/songs/urls.py
+++ core/songs/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
-from songs.views import AlbumSongView, ArtistSongView, SongDetailView, SongListView
+from songs.views import (
+    AlbumSongView,
+    ArtistSongView,
+    ManagerSongView,
+    SongDetailView,
+    SongListView,
+)
 
 urlpatterns = [
     # Songs
-    path("", SongListView.as_view(), name="song-list"),
     path("<str:pk>/", SongDetailView.as_view(), name="song-detail"),
     path("artists/<str:pk>/", ArtistSongView.as_view(), name="artist-song"),
     path("albums/<str:pk>/", AlbumSongView.as_view(), name="album-song"),
+    path("managers/<str:manager_id>/", ManagerSongView.as_view(), name="manager-songs"),
+    path("", SongListView.as_view(), name="song-list"),
 ]
